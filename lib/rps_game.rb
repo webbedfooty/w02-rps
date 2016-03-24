86943d8c262a595b9558c8ae3a18d341de6f153a
Patrick Wiechert <patrickwiechert@yahoo.com>
2016-03-24 14:27:03 -0500

    added in outcomes but they don't print

--- lib/rps_game.rb
+++ lib/rps_game.rb
@@ -8,23 +8,49 @@ puts "\nPlayer One Ready?"
 puts "\nOne! Two! Three! Shoot!"
 
 computer_throw = ["rock", "paper", "scissors"]
-throw = "O"
-while throw != "R" || throw != "r" || throw != "P" || throw != "p" || throw != "S" || throw != "s"
-  throw = gets.chomp
+#throw = "O"
+#while throw != "R" || throw != "r" || throw != "P" || throw != "p" || throw != "S" || throw != "s"
+throw = gets.chomp
 
 if throw == "R" || throw == "r"
     p1_throw = "rock"
     puts "\nYou threw Rock!"
-    puts "The computer threw #{computer.sample}!"
+    cpu_throw = computer_throw.sample
+    puts "The computer threw #{cpu_throw}!"
+
   elsif throw == "P" || throw == "p"
     p1_throw = "paper"
     puts "\nYou threw Paper!"
-    puts "The computer threw #{computer.sample}!"
+    cpu_throw = computer_throw.sample
+    puts "The computer threw #{cpu_throw}!"
+
   elsif throw == "S" || throw == "s"
     p1_throw = "scissors"
     puts "\nYou threw Scissors!"
-    puts "The computer threw #{computer.sample}!"
-  elsif
+    cpu_throw = computer_throw.sample
+    puts "The computer threw #{cpu_throw}!"
+
+  else
     puts "\nI don't know what that is, try again"
 end
+#end
+
+def throw_outcome(p1_throw, cpu_throw)
+  if p1_throw == cpu_throw
+    puts "\nTIE!"
+    outcome = "Throw again!"
+  elsif p1_throw == "rock" && cpu_throw == "scissors"
+    outcome = "\nYou win!"
+  elsif p1_throw == "paper" && cpu_throw == "rock"
+    outcome = "\nYou win!"
+  elsif p1_throw == "scissors" && cpu_throw == "paper"
+    outcome = "\nYou win!"
+  elsif p1_throw == "rock" && cpu_throw == "paper"
+    outcome = "\nYou lose."
+  elsif p1_throw == "paper" && cpu_throw == "scissors"
+    outcome = "\nYou lose."
+  else p1_throw == "scissors" && cpu_throw == "rock"
+    outcome = "\nYou lose."
+  end
+  outcome
 end
