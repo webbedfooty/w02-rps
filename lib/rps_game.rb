f8d0730d91dd38d70ad289cc16230c92e5dde4db
Patrick Wiechert <patrickwiechert@yahoo.com>
2016-03-24 12:47:49 -0500

    added in dialogue to make choice

--- lib/rps_game.rb
+++ lib/rps_game.rb
@@ -1,5 +1,21 @@
 # Basic: Rock Paper Scissors
-puts "Welcome to\n"
+puts "\n Welcome to"
 puts "Rock!"
 puts "Paper!"
-puts "Scissors!"
+puts "Scissors!\n"
+puts "To select Rock, enter R"
+puts "To select Paper, enter P"
+puts "To select Scissors, enter S\n"
+puts "Player One Ready?\n"
+puts "One! Two! Three! Shoot!"
+throw = gets.chomp
+
+if throw == "R" || throw == "r"
+  puts "You threw Rock!"
+elsif throw == "P" || throw == "p"
+  puts "You threw Paper!"
+elsif throw == "S" || throw == "s"
+  puts "You threw Scissors!"
+else
+  puts "I don't know what that is, try again"
+end
