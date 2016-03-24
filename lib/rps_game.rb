2f20a45b5aebe15e274da64eea6e7d7364c68a53
Patrick Wiechert <patrickwiechert@yahoo.com>
2016-03-24 13:40:14 -0500

    added in responses to selection

--- lib/rps_game.rb
+++ lib/rps_game.rb
@@ -1,21 +1,23 @@
 # Basic: Rock Paper Scissors
-puts "\n Welcome to"
-puts "Rock!"
-puts "Paper!"
-puts "Scissors!\n"
-puts "To select Rock, enter R"
+puts "\nWelcome to Rock! Paper! Scissors!"
+
+puts "\nTo select Rock, enter R"
 puts "To select Paper, enter P"
-puts "To select Scissors, enter S\n"
-puts "Player One Ready?\n"
-puts "One! Two! Three! Shoot!"
-throw = gets.chomp
+puts "To select Scissors, enter S"
+puts "\nPlayer One Ready?"
+puts "\nOne! Two! Three! Shoot!"
+
+throw = "O"
+while throw != "R" || throw != "r" || throw != "P" || throw != "p" || throw != "S" || throw != "s"
+  throw = gets.chomp
 
 if throw == "R" || throw == "r"
-  puts "You threw Rock!"
-elsif throw == "P" || throw == "p"
-  puts "You threw Paper!"
-elsif throw == "S" || throw == "s"
-  puts "You threw Scissors!"
-else
-  puts "I don't know what that is, try again"
+    puts "\nYou threw Rock!"
+  elsif throw == "P" || throw == "p"
+    puts "\nYou threw Paper!"
+  elsif throw == "S" || throw == "s"
+    puts "\nYou threw Scissors!"
+  elsif
+    puts "\nI don't know what that is, try again"
+end
 end
