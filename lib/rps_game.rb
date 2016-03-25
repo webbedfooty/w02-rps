ce57e3edcbf578ad3d5ac6d15084be07e3e86cef
Patrick Wiechert <patrickwiechert@yahoo.com>
2016-03-24 21:04:08 -0500

    Trying to clean it up and stop the looping

--- lib/rps_game.rb
+++ lib/rps_game.rb
@@ -1,6 +1,5 @@
 # Basic: Rock Paper Scissors
 puts "\nWelcome to Rock! Paper! Scissors!"
-
 puts "\nTo select Rock, enter R"
 puts "To select Paper, enter P"
 puts "To select Scissors, enter S"
@@ -8,42 +7,17 @@ puts "\nPlayer One Ready?"
 puts "\nOne! Two! Three! Shoot!"
 
 computer_throw = ["rock", "paper", "scissors"]
-throw = "J"
-def player1_throw(throw)
-while throw != "R" || throw != "r" || throw != "P" || throw != "p" || throw != "S" || throw != "s"
-  throw = gets.chomp
-if throw == "R" || throw == "r"
+throw = gets.chomp.downcase
+if throw == "r"
     p1_throw = "rock"
     puts "\nYou threw Rock!"
-  elsif throw == "P" || throw == "p"
+  elsif throw == "p"
     p1_throw = "paper"
     puts "\nYou threw Paper!"
-  elsif throw == "S" || throw == "s"
+  elsif throw == "s"
     p1_throw = "scissors"
     puts "\nYou threw Scissors!"
   else
     puts "\nI don't know what that is, try again"
+    throw = gets.chomp.downcase
 end
-player1_throw(throw)
-end
-
-def throw_outcome(p1_throw, cpu_throw)
-  if p1_throw == cpu_throw
-    puts "\nTIE!"
-    outcome = "Throw again!"
-  elsif p1_throw == "rock" && cpu_throw == "scissors"
-    outcome = "\nYou win!"
-  elsif p1_throw == "paper" && cpu_throw == "rock"
-    outcome = "\nYou win!"
-  elsif p1_throw == "scissors" && cpu_throw == "paper"
-    outcome = "\nYou win!"
-  elsif p1_throw == "rock" && cpu_throw == "paper"
-    outcome = "\nYou lose."
-  elsif p1_throw == "paper" && cpu_throw == "scissors"
-    outcome = "\nYou lose."
-  else p1_throw == "scissors" && cpu_throw == "rock"
-    outcome = "\nYou lose."
-  end
-  outcome
-end
-throw_outcome(p1_throw, cpu_throw)
