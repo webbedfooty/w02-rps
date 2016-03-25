574479146ebb7630659cac764f8ca7a09c1b6e10
Patrick Wiechert <patrickwiechert@yahoo.com>
2016-03-24 21:12:26 -0500

    figured out how to stop loop

--- lib/rps_game.rb
+++ lib/rps_game.rb
@@ -6,18 +6,21 @@ puts "To select Scissors, enter S"
 puts "\nPlayer One Ready?"
 puts "\nOne! Two! Three! Shoot!"
 
-computer_throw = ["rock", "paper", "scissors"]
 throw = gets.chomp.downcase
+while throw != "r" && throw != "p" && throw != "s"
+  puts "\nI don't know what that is, try again"
+  throw = gets.chomp.downcase
+end
+
 if throw == "r"
     p1_throw = "rock"
     puts "\nYou threw Rock!"
   elsif throw == "p"
     p1_throw = "paper"
     puts "\nYou threw Paper!"
-  elsif throw == "s"
+  else throw == "s"
     p1_throw = "scissors"
     puts "\nYou threw Scissors!"
-  else
-    puts "\nI don't know what that is, try again"
-    throw = gets.chomp.downcase
 end
+rps = ["rock", "paper", "scissors"]
+computer_throw = rps.sample
