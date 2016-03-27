24663db5eaa19d52dbd5273fb3528195f31497ff
Patrick Wiechert <patrickwiechert@yahoo.com>
2016-03-27 18:18:03 -0500

    updated

--- main_rps.rb
+++ main_rps.rb
@@ -25,24 +25,12 @@ def game(player_one, player_two, n_rounds)
   def player_one_throw#(p1_throw)
     puts "\nPlayer One Ready?   One! Two! Three! Shoot!"
       p1_throw = gets.chomp.downcase
-    while p1_throw != "r" && throw != "p" && throw != "s"
+    while p1_throw != "r" && p1_throw != "p" && p1_throw != "s"
       puts "\nI don't know what that is - please try again"
       #p1_throw = gets.chomp.downcase
     end
     p1_throw
-  end
-#######################################################
-  def player_two_throw
-    puts "\nPlayer Two Ready?   One! Two! Three! Shoot!"
-      p2_throw = gets.chomp.downcase
-    while p2_throw != "r" && throw != "p" && throw != "s"
-      puts "\nI don't know what that is - please try again"
-      #p2_throw = gets.chomp.downcase
-    end
-    p2_throw
-  end
-#######################################################
-  def throw_results(p1_throw, p2_throw)
+
     if p1_throw == "r"
       puts "\nPlayer One threw Rock!"
     elsif p1_throw == "p"
@@ -51,14 +39,27 @@ def game(player_one, player_two, n_rounds)
       puts "\nPlayer One threw Scissors!"
     end
 
-    if p2_throw == "r"
-      puts "\nPlayer Two threw Rock!"
-    elsif p2_throw == "p"
-      puts "\nPlayer Two threw Paper!"
-    else p2_throw == "s"
-      puts "\nPlayer Two threw Scissors!"
-    end
   end
+#######################################################
+  def player_two_throw
+    puts "\nPlayer Two Ready?   One! Two! Three! Shoot!"
+      p2_throw = gets.chomp.downcase
+    while p2_throw != "r" && p2_throw != "p" && p2_throw != "s"
+      puts "\nI don't know what that is - please try again"
+      #p2_throw = gets.chomp.downcase
+    end
+    p2_throw
+
+    if p2_throw == "r"
+      puts "\nPlayer One threw Rock!"
+    elsif p2_throw == "p"
+      puts "\nPlayer One threw Paper!"
+    else p2_throw == "s"
+      puts "\nPlayer One threw Scissors!"
+    end
+
+  end
+
 #######################################################
   p1_record = 0
   p2_record = 0
@@ -85,7 +86,7 @@ x = 0
   while x != n_rounds
     player_one_throw
     player_two_throw
-    throw_results(p1_throw, p2_throw)
+    #throw_results(p1_throw, p2_throw)
     throw_score(p1_throw, p2_throw)
     x+=1
     puts "The score is #{player_one} #{p1_record} to #{player_two} #{p2_record}"
