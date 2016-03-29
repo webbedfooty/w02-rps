af683331df7d29d4405db11e28343935db28f455
Patrick Wiechert <patrickwiechert@yahoo.com>
2016-03-28 21:12:46 -0500

    closer

--- main_rps.rb
+++ main_rps.rb
@@ -13,34 +13,30 @@ end
 #######################################################
 
 
-def player_one_throw
+def player_one_throw(player_one.throw)
 
-  if p1_throw == "r"
+  if player_one.throw == "r"
     puts "\nPlayer One threw Rock!"
-  elsif p1_throw == "p"
+  elsif player_one.throw == "p"
     puts "\nPlayer One threw Paper!"
-  else p1_throw == "s"
+  else player_one.throw == "s"
     puts "\nPlayer One threw Scissors!"
   end
   p1_throw
 end
 #######################################################
-def player_two_throw
+def player_two_throw(player_two.throw)
 
-  if p2_throw == "r"
+  if player_two.throw == "r"
     puts "\nPlayer Two threw Rock!"
-  elsif p2_throw == "p"
+  elsif player_two.throw == "p"
     puts "\nPlayer Two threw Paper!"
-  else p2_throw == "s"
+  else player_two.throw == "s"
     puts "\nPlayer Two threw Scissors!"
   end
-  p2_throw
+
 end
 
-
-#######################################################
-x = 0
-#############################################
 def game(player_one, player_two, n_rounds)
   x = 0
   p1_record = 0
@@ -54,12 +50,18 @@ def game(player_one, player_two, n_rounds)
 end
 player_one = Player.new
 player_two = Player.new
+
+player_one.wins = 0
+player_two.wins = 0
+
+x = 0
+
 puts "\nWelcome to Rock! Paper! Scissors!"
 puts "\nPlayer One, please enter your name"
 player_one.name = gets.chomp.capitalize
 puts "\nPlayer Two, please enter your name"
 player_two.name = gets.chomp.capitalize
-Player.new
+
 puts "How many rounds would you like to play? "
   n_rounds = gets.chomp.to_i
   rounds_to_play(n_rounds)
@@ -68,7 +70,6 @@ puts "\nTo select Rock, enter r"
 puts "To select Paper, enter p"
 puts "To select Scissors, enter s"
 
-def throw
   puts "\nPlayer One Ready?   One! Two! Three! Shoot!"
   player_one.throw = gets.chomp.downcase
   while player_one.throw != "r" &&
@@ -77,6 +78,7 @@ def throw
       puts "\nI don't know what that is - please try again"
     player_one.throw = gets.chomp.downcase
   end
+    puts player_one_throw(player_one.throw)
 
   puts "\nPlayer Two Ready?   One! Two! Three! Shoot!"
   player_two.throw = gets.chomp.downcase
@@ -86,9 +88,8 @@ def throw
       puts "\nI don't know what that is - please try again"
       player_two.throw = gets.chomp.downcase
   end
-end
+    puts player_two_throw(player_two.throw)
 
-def throw_score(player_one.throw, player_two.throw)
     if player_one.throw == player_two.throw
       puts "It's a tie!"
         throw
@@ -105,5 +106,6 @@ def throw_score(player_one.throw, player_two.throw)
         puts "Player Two wins!"
           player_two.wins += 1
     end
-  puts "The score is #{player_one.name} #{p1_record} to #{player_two.name} #{p2_record}"
-  end
+puts "The score is #{player_one.name} #{player_one.wins}
+  to
+  #{player_two.name} #{player_two.wins}"
